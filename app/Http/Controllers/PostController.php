db295edc8771e5b0172aa869eedb38f3c79e826a
Gerson <gedacaba@gmail.com>
2022-03-20 18:32:10 -0500

    agragando controlador y vista para usuario

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index() 
+    public function index()
     {
         return view('index', [
             'posts' => Post::latest()->paginate()
+        ]);
+    }
+
+    public function showuser()
+    {
+        return view('users', [
+            'users' => User::latest()->paginate()
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\PostController::class, 'index']);
+Route::get('usuarios', [App\Http\Controllers\PostController::class, 'showuser']);
